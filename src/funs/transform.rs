e06117f29cef0a71ed3132cb4283a7a9bbb0d05d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-11-08 11:48:29 +1000

    Add Radian and Degree types

--- src/funs/transform.rs
+++ src/funs/transform.rs
@@ -1,17 +1,21 @@
 use num::Num;
 use ncast::*;
-use funs::exp::*;
+use ntrait::Float;
+use funs::exp::Exp;
 use funs::trig::*;
 use matrix::Mat4;
 
-pub fn mat4_from_rotation<T:Copy Num NumCast Exp Trig>(angle: T, axis: Vec3<T>) -> Mat4<T> {
-    let angle_rad = radians(&angle);
-    let c = cos(&angle_rad);
-    let s = sin(&angle_rad);
-    
+pub fn mat4_from_rotation
+<
+    ThetaT:NumCast Float,
+    T:Copy Num NumCast
+>
+(theta: Radians<ThetaT>, axis: Vec3<T>) -> Mat4<T> {
+    let c:  T = cos(&theta);
+    let s:  T = sin(&theta);
     let _0: T = cast(0);
     let _1: T = cast(1);
-    let t = _1 - c;
+    let t:  T = _1 - c;
     
     Mat4::new(t * axis.x * axis.x + c,          t * axis.x * axis.y + s * axis.z, t * axis.x * axis.z - s * axis.y, _0,
               t * axis.x * axis.y - s * axis.z, t * axis.y * axis.y + c,          t * axis.y * axis.z + s * axis.x, _0,
