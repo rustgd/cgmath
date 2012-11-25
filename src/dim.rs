b753d27ac84527931c3336c8429286fc7cf62a05
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-11-25 22:21:27 +1000

    Remove useless import

--- src/dim.rs
+++ src/dim.rs
@@ -1,5 +1,3 @@
-use core::cmp::Eq;
-
 pub trait Dimensional<T>: Index<uint, T> {
     static pure fn dim() -> uint;       // dim and size_of are pretty useless at the moment due to
     static pure fn size_of() -> uint;   // how Rust's static methods are currently implemented
