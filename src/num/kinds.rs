9d03050ed1341f7875cf7da2682b27cb1328a5d2
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-12-08 21:15:52 +1000

    Add size_of and bits methods

--- src/num/kinds.rs
+++ src/num/kinds.rs
@@ -18,6 +18,9 @@ pub trait Number: Eq, Num, NumConv, Ord {
      */
     static pure fn from<T:Number>(n: T) -> self;
     
+    static pure fn size_of() -> uint;
+    static pure fn bits() -> uint;
+    
     static pure fn zero() -> self;      /// The additive identity
     static pure fn one()  -> self;      // The multiplicative identity
 }
@@ -27,6 +30,9 @@ pub impl u8: Number {
      * Construct a `u8` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> u8 { n.to_u8() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<u8>() }
+    #[inline(always)] static pure fn bits() -> uint { 8 }
     
     #[inline(always)] static pure fn zero() -> u8 { 0u8 }       /// 0u8
     #[inline(always)] static pure fn one()  -> u8 { 1u8 }       //  1u8
@@ -38,6 +44,9 @@ pub impl u16: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> u16 { n.to_u16() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<u16>() }
+    #[inline(always)] static pure fn bits() -> uint { 16 }
+    
     #[inline(always)] static pure fn zero() -> u16 { 0u16 }     /// 0u16
     #[inline(always)] static pure fn one()  -> u16 { 1u16 }     //  1u16
 }
@@ -47,6 +56,9 @@ pub impl u32: Number {
      * Construct a `u32` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> u32 { n.to_u32() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<u32>() }
+    #[inline(always)] static pure fn bits() -> uint { 32 }
     
     #[inline(always)] static pure fn zero() -> u32 { 0u32 }     /// 0u32
     #[inline(always)] static pure fn one()  -> u32 { 1u32 }     //  1u32
@@ -58,6 +70,9 @@ pub impl u64: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> u64 { n.to_u64() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<u64>() }
+    #[inline(always)] static pure fn bits() -> uint { 64 }
+    
     #[inline(always)] static pure fn zero() -> u64 { 0u64 }     /// 0u64
     #[inline(always)] static pure fn one()  -> u64 { 1u64 }     //  1u64
 }
@@ -67,6 +82,9 @@ pub impl uint: Number {
      * Construct a `uint` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> uint { n.to_uint() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<uint>() }
+    #[inline(always)] static pure fn bits() -> uint { sys::size_of::<uint>() * 8 }
     
     #[inline(always)] static pure fn zero() -> uint { 0u }      /// 0u
     #[inline(always)] static pure fn one()  -> uint { 1u }      //  1u
@@ -78,6 +96,9 @@ pub impl i8: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> i8 { n.to_i8() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<i8>() }
+    #[inline(always)] static pure fn bits() -> uint { 8 }
+    
     #[inline(always)] static pure fn zero() -> i8 { 0i8 }       /// 0i8
     #[inline(always)] static pure fn one()  -> i8 { 1i8 }       //  1i8
 }
@@ -87,6 +108,9 @@ pub impl i16: Number {
      * Construct an `i16` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> i16 { n.to_i16() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<i16>() }
+    #[inline(always)] static pure fn bits() -> uint { 16 }
     
     #[inline(always)] static pure fn zero() -> i16 { 0i16 }     /// 0i16
     #[inline(always)] static pure fn one()  -> i16 { 1i16 }     //  1i16
@@ -98,6 +122,9 @@ pub impl i32: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> i32 { n.to_i32() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<i32>() }
+    #[inline(always)] static pure fn bits() -> uint { 32 }
+    
     #[inline(always)] static pure fn zero() -> i32 { 0i32 }     /// 0i32
     #[inline(always)] static pure fn one()  -> i32 { 1i32 }     //  1i32
 }
@@ -107,6 +134,9 @@ pub impl i64: Number {
      * Construct an `i64` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> i64 { n.to_i64() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<i64>() }
+    #[inline(always)] static pure fn bits() -> uint { 64 }
     
     #[inline(always)] static pure fn zero() -> i64 { 0i64 }     /// 0i64
     #[inline(always)] static pure fn one()  -> i64 { 1i64 }     //  1i64
@@ -118,6 +148,9 @@ pub impl int: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> int { n.to_int() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<int>() }
+    #[inline(always)] static pure fn bits() -> uint { sys::size_of::<int>() * 8 }
+    
     #[inline(always)] static pure fn zero() -> int { 0 }        /// 0
     #[inline(always)] static pure fn one()  -> int { 1 }        //  1
 }
@@ -127,6 +160,9 @@ pub impl f32: Number {
      * Construct a `f32` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> f32 { n.to_f32() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<f32>() }
+    #[inline(always)] static pure fn bits() -> uint { 32 }
     
     #[inline(always)] static pure fn zero() -> f32 { 0f32 }     /// 0f32
     #[inline(always)] static pure fn one()  -> f32 { 1f32 }     //  1f32
@@ -138,6 +174,9 @@ pub impl f64: Number {
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> f64 { n.to_f64() }
     
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<f64>() }
+    #[inline(always)] static pure fn bits() -> uint { 64 }
+    
     #[inline(always)] static pure fn zero() -> f64 { 0f64 }     /// 0f64
     #[inline(always)] static pure fn one()  -> f64 { 1f64 }     //  1f64
 }
@@ -147,6 +186,9 @@ pub impl float: Number {
      * Construct a `float` from the type `T:Number`
      */
     #[inline(always)] static pure fn from<T:Number>(n: T) -> float { n.to_float() }
+    
+    #[inline(always)] static pure fn size_of() -> uint { sys::size_of::<float>() }
+    #[inline(always)] static pure fn bits() -> uint { sys::size_of::<float>() * 8 }
     
     #[inline(always)] static pure fn zero() -> float { 0f }     /// 0f
     #[inline(always)] static pure fn one()  -> float { 1f }     //  1f
