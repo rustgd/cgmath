d81157fba54e5aa046d474314127e99e3de87d97
kvark <kvarkus@gmail.com>
2013-11-01 07:09:57 -0400

    Generalized Ray & Transform over dimensions

--- src/cgmath/transform.rs
+++ src/cgmath/transform.rs
@@ -15,18 +15,25 @@
 
 use matrix::Mat4;
 use point::{Point,Point3};
-use ray::Ray3;
+use ray::Ray;
 use rotation::Rotation3;
 use vector::{Vector,Vec3};
 
+/// A trait of affine transformation, that can be applied to points or vectors
+pub trait Transform
+<
+    S: Primitive,
+    Slice,
+    V: Vector<S,Slice>,
+    P: Point<S,V,Slice>
+>
+{
+    fn transform_vec(&self, vec: &V) -> V;
+    fn transform_point(&self, point: &P) -> P;
 
-pub trait Transform3<S> {
-    fn transform_vec3(&self, vec: &Vec3<S>) -> Vec3<S>;
-    fn transform_point3(&self, point: &Point3<S>) -> Point3<S>;
-    
     #[inline]
-    fn transform_ray3(&self, ray: &Ray3<S>) -> Ray3<S>    {
-        Ray3::new( self.transform_point3(&ray.origin), self.transform_vec3(&ray.direction) )
+    fn transform_ray(&self, ray: &Ray<P,V>) -> Ray<P,V>    {
+        Ray::new( self.transform_point(&ray.origin), self.transform_vec(&ray.direction) )
     }
 }
 
@@ -37,27 +44,27 @@ pub struct AffineMatrix3<S> {
 
 /// A transformation in three dimensions consisting of a rotation,
 /// displacement vector and scale amount.
-pub struct Transform3D<S, R> {
+pub struct Transform3<S, R> {
     rot: R,
     disp: Vec3<S>,
     scale: S,
 }
 
-impl<S: Float, R: Rotation3<S>> Transform3D<S, R> {
+impl<S: Float, R: Rotation3<S>> Transform3<S, R> {
     #[inline]
-    pub fn new(rot: R, disp: Vec3<S>, scale: S) -> Transform3D<S, R> {
-        Transform3D { rot: rot, disp: disp, scale: scale }
+    pub fn new(rot: R, disp: Vec3<S>, scale: S) -> Transform3<S, R> {
+        Transform3 { rot: rot, disp: disp, scale: scale }
     }
 }
 
-impl <S: Float, R: Rotation3<S>> Transform3<S> for Transform3D<S,R>   {
+impl <S: Float, R: Rotation3<S>> Transform<S, [S, .. 3], Vec3<S>, Point3<S>> for Transform3<S,R>   {
     #[inline]
-    fn transform_vec3(&self, vec: &Vec3<S>) -> Vec3<S>   {
+    fn transform_vec(&self, vec: &Vec3<S>) -> Vec3<S>   {
         self.rot.rotate_vec3( &vec.mul_s( self.scale.clone() ))
     }
 
     #[inline]
-    fn transform_point3(&self, point: &Point3<S>) -> Point3<S>   {
+    fn transform_point(&self, point: &Point3<S>) -> Point3<S>   {
         self.rot.rotate_point3( &point.mul_s( self.scale.clone() )).add_v( &self.disp )
     }
 }
