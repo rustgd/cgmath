e9fcf971da94446c126e45b211912b39a1eabfca
Lennart <lennartj@stanford.edu>
2014-01-26 21:47:03 -0800

    Fix comments on Quat::from_angle_{x|y|z} functions

--- src/cgmath/quaternion.rs
+++ src/cgmath/quaternion.rs
@@ -47,21 +47,21 @@ Quat<S> {
         Quat { s: s, v: v }
     }
 
-    /// Create a matrix from a rotation around the `x` axis (pitch).
+    /// Create a quaternion from a rotation around the `x` axis (pitch).
     #[inline]
     pub fn from_angle_x(theta: Rad<S>) -> Quat<S> {
         let (s, c) = sin_cos(theta.mul_s(cast(0.5).unwrap()));
         Quat::new(c, s, zero(), zero())
     }
 
-    /// Create a matrix from a rotation around the `y` axis (yaw).
+    /// Create a quaternion from a rotation around the `y` axis (yaw).
     #[inline]
     pub fn from_angle_y(theta: Rad<S>) -> Quat<S> {
         let (s, c) = sin_cos(theta.mul_s(cast(0.5).unwrap()));
         Quat::new(c, zero(), s, zero())
     }
 
-    /// Create a matrix from a rotation around the `z` axis (roll).
+    /// Create a quaternion from a rotation around the `z` axis (roll).
     #[inline]
     pub fn from_angle_z(theta: Rad<S>) -> Quat<S> {
         let (s, c) = sin_cos(theta.mul_s(cast(0.5).unwrap()));
