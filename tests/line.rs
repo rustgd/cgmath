814b845de7687f6d89801cdde9c423b19f3d23d1
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2014-05-29 21:42:04 -0400

    Update line intersection
    *Change line<>line intersection to be ray<>line.
    *Update tests accordingly
    *Clean up and simplify intersection logic

--- tests/line.rs
+++ tests/line.rs
@@ -19,65 +19,54 @@ extern crate cgmath;
 
 use cgmath::line::*;
 use cgmath::point::*;
+use cgmath::ray::*;
+use cgmath::vector::*;
 use cgmath::intersect::Intersect;
 
 #[test]
 fn test_line_intersection() {
-    // collinear, origins pointing towards each other, first intersection
-    // from l1.origin is in an endpoint in l2
-    let l1 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(10.0f64, 0.0f64));
-    let l2 = Line::new(Point2::new(1.5f64, 0.0f64), Point2::new(0.5f64, 0.0f64));
-    assert_eq!((l1, l2).intersection(), Some(Point2::new(0.5f64, 0.0f64)));
+    // collinear, intersection is line dest
+    let r1 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(0.25, 0.0));
+    let l1 = Line::new(Point2::new(1.5f32, 0.0), Point2::new(0.5, 0.0));
+    assert_eq!((r1, l1).intersection(), Some(Point2::new(0.5, 0.0)));
 
-    // collinear, first intersection from p1.origin is at p1.origin itself
-    let l3 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(10.0f64, 0.0f64));
-    let l4 = Line::new(Point2::new(-11.0f64, 0.0f64), Point2::new(1.0f64, 0.0f64));
-    assert_eq!((l3, l4).intersection(), Some(Point2::new(0.0f64, 0.0f64)));
+    // collinear, intersection is at ray origin
+    let r2 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(5.0, 0.0));
+    let l2 = Line::new(Point2::new(-11.0f32, 0.0), Point2::new(1.0, 0.0));
+    assert_eq!((r2, l2).intersection(), Some(Point2::new(0.0, 0.0)));
 
-    // no intersection
-    let l5 = Line::new(Point2::new(5.0f64, 5.0f64), Point2::new(10.0f64, 6.0f64));
-    let l6 = Line::new(Point2::new(5.0f64, 4.8f64), Point2::new(10.0f64, 4.1f64));
-    assert_eq!((l5, l6).intersection(), None); // no intersection
-
-    // collinear, origins pointing same direction
-    let l7 = Line::new(Point2::new(0.0f64, 1.0f64), Point2::new(0.0f64, 0.0f64));
-    let l8 = Line::new(Point2::new(0.0f64, 0.5f64), Point2::new(0.0f64, -0.5f64));
-    assert_eq!((l7, l8).intersection(), Some(Point2::new(0.0f64, 0.5f64)));
+    // collinear, intersection is line origin
+    let r3 = Ray::new(Point2::new(0.0f32, 1.0), Vector2::new(0.0, -0.25));
+    let l3 = Line::new(Point2::new(0.0f32, 0.5), Point2::new(0.0, -0.5));
+    assert_eq!((r3, l3).intersection(), Some(Point2::new(0.0, 0.5)));
 
     // collinear, no overlap
-    let l9 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(3.0f64, 0.0f64));
-    let l10 = Line::new(Point2::new(10.0f64, 0.0f64), Point2::new(5.0f64, 0.0f64));
-    assert_eq!((l9, l10).intersection(), None);
+    let r4 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(3.0, 0.0));
+    let l4 = Line::new(Point2::new(-10.0f32, 0.0), Point2::new(-5.0, 0.0));
+    assert_eq!((r4, l4).intersection(), None);
 
-    // intersection found
-    let l11 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(10.0f64, 10.0f64));
-    let l12 = Line::new(Point2::new(0.0f64, 10.0f64), Point2::new(10.0f64, 0.0f64));
-    assert_eq!((l11, l12).intersection(), Some(Point2::new(5.0f64, 5.0f64)));
+    // no intersection
+    let r5 = Ray::new(Point2::new(5.0f32, 5.0), Vector2::new(40.0, 8.0));
+    let l5 = Line::new(Point2::new(5.0f32, 4.8), Point2::new(10.0, 4.1));
+    assert_eq!((r5, l5).intersection(), None); // no intersection
 
-    // special case of both lines being the same point
-    let l13 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(0.0f64, 0.0f64));
-    let l14 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(0.0f64, 0.0f64));
-    assert_eq!((l13, l14).intersection(), Some(Point2::new(0.0f64, 0.0f64)));
+    // non-collinear intersection
+    let r6 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(10.0, 10.0));
+    let l6 = Line::new(Point2::new(0.0f32, 10.0), Point2::new(10.0, 0.0));
+    assert_eq!((r6, l6).intersection(), Some(Point2::new(5.0, 5.0)));
 
-    // both lines are points that are distinct
-    let l15 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(0.0f64, 0.0f64));
-    let l16 = Line::new(Point2::new(1.0f64, 0.0f64), Point2::new(1.0f64, 0.0f64));
-    assert_eq!((l15, l16).intersection(), None);
+    // line is a point that does not intersect
+    let r7 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(1.0, 1.0));
+    let l7 = Line::new(Point2::new(1.0f32, 0.0), Point2::new(1.0, 0.0));
+    assert_eq!((r7, l7).intersection(), None);
 
-    // one line is a point that intersects the other segment
-    let l15 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(10.0f64, 0.0f64));
-    let l16 = Line::new(Point2::new(3.0f64, 0.0f64), Point2::new(3.0f64, 0.0f64));
-    assert_eq!((l15, l16).intersection(), Some(Point2::new(3.0f64, 0.0f64)));
+    // line is a point that does intersect
+    let r8 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(1.0, 0.0));
+    let l8 = Line::new(Point2::new(3.0f32, 0.0), Point2::new(3.0, 0.0));
+    assert_eq!((r8, l8).intersection(), Some(Point2::new(3.0, 0.0)));
 
-    // one line is a point that is collinear but does not intersect with
-    // the other line
-    let l17 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(0.0f64, 0.0f64));
-    let l18 = Line::new(Point2::new(1.0f64, 0.0f64), Point2::new(3.0f64, 0.0f64));
-    assert_eq!((l17, l18).intersection(), None);
-
-    // one line is a point that is not collinear but does not intersect
-    // with the other line
-    let l19 = Line::new(Point2::new(0.0f64, 0.0f64), Point2::new(0.0f64, 0.0f64));
-    let l20 = Line::new(Point2::new(1.0f64, 0.0f64), Point2::new(2.0f64, 10.0f64));
-    assert_eq!((l19, l20).intersection(), None);
+    // line is a collinear point but no intersection
+    let r9 = Ray::new(Point2::new(0.0f32, 0.0), Vector2::new(1.0, 0.0));
+    let l9 = Line::new(Point2::new(-1.0f32, 0.0), Point2::new(-1.0, 0.0));
+    assert_eq!((r9, l9).intersection(), None);
 }
