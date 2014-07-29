1e1f60379e4461778de0f483b5e40f4bc239f8ab
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-07-30 04:06:31 +1000

    Deprecate element and column accessors in favour of index operators
    Also removes some unnecessary `clone` calls.

--- src/array.rs
+++ src/array.rs
@@ -17,30 +17,42 @@ use std::mem;
 use std::ptr;
 
 /// An array containing elements of type `Element`
-pub trait Array1<Element: Copy> {
+pub trait Array1<Element: Copy>: Index<uint, Element> + IndexMut<uint, Element> {
     /// Get the pointer to the first element of the array.
-    fn ptr<'a>(&'a self) -> &'a Element;
+    fn ptr<'a>(&'a self) -> &'a Element {
+        &(*self)[0]
+    }
 
     /// Get a mutable pointer to the first element of the array.
-    fn mut_ptr<'a>(&'a mut self) -> &'a mut Element;
+    fn mut_ptr<'a>(&'a mut self) -> &'a mut Element {
+        &mut (*self)[0]
+    }
 
     /// Get a shared reference to the `i`th value.
-    fn i(&self, i: uint) -> Element;
+    #[deprecated = "Use index operator instead"]
+    #[inline]
+    fn i<'a>(&'a self, i: uint) -> &'a Element {
+        &(*self)[i]
+    }
 
     /// Get a mutable reference to the `i`th value.
-    fn mut_i<'a>(&'a mut self, i: uint) -> &'a mut Element;
+    #[deprecated = "Use index operator instead"]
+    #[inline]
+    fn mut_i<'a>(&'a mut self, i: uint) -> &'a mut Element {
+        &mut (*self)[i]
+    }
 
     #[inline]
-    /// Swap the elements at indices `a` and `b` in-place.
-    fn swap_i(&mut self, a: uint, b: uint) {
+    /// Swap the elements at indices `i` and `j` in-place.
+    fn swap_i(&mut self, i: uint, j: uint) {
         // Yeah, ok borrow checker – I know what I'm doing here
-        unsafe { ptr::swap(self.mut_i(a), self.mut_i(b)) };
+        unsafe { ptr::swap(&mut (*self)[i], &mut (*self)[j]) };
     }
 
     /// Replace an element in the array.
     #[inline]
     fn replace_i(&mut self, i: uint, src: Element) -> Element {
-        mem::replace(self.mut_i(i), src)
+        mem::replace(&mut (*self)[i], src)
     }
 
     /// Apply a function to each element.
@@ -48,29 +60,42 @@ pub trait Array1<Element: Copy> {
 }
 
 /// A column-major array
-pub trait Array2<Column: Array1<Element>, Row: Array1<Element>, Element: Copy> {
+pub trait Array2<Column: Array1<Element>, Row: Array1<Element>, Element: Copy>:
+        Index<uint, Column> + IndexMut<uint, Column> {
     /// Get the pointer to the first element of the array.
-    fn ptr<'a>(&'a self) -> &'a Element;
+    fn ptr<'a>(&'a self) -> &'a Element {
+        &(*self)[0][0]
+    }
 
     /// Get a mutable pointer to the first element of the array.
-    fn mut_ptr<'a>(&'a mut self) -> &'a mut Element;
+    fn mut_ptr<'a>(&'a mut self) -> &'a mut Element {
+        &mut (*self)[0][0]
+    }
 
     /// Get a shared reference to a column of this array.
-    fn c<'a>(&'a self, c: uint) -> &'a Column;
+    #[deprecated = "Use index operator instead"]
+    #[inline]
+    fn c<'a>(&'a self, c: uint) -> &'a Column {
+        &(*self)[c]
+    }
 
     /// Get a mutable reference to a column of this array.
-    fn mut_c<'a>(&'a mut self, c: uint) -> &'a mut Column;
+    #[deprecated = "Use index operator instead"]
+    #[inline]
+    fn mut_c<'a>(&'a mut self, c: uint) -> &'a mut Column {
+        &mut (*self)[c]
+    }
 
     /// Swap two columns of this array.
     #[inline]
     fn swap_c(&mut self, a: uint, b: uint) {
-        unsafe { ptr::swap(self.mut_c(a), self.mut_c(b)) };
+        unsafe { ptr::swap(&mut (*self)[a], &mut (*self)[b]) };
     }
 
     /// Replace a column in the array.
     #[inline]
     fn replace_c(&mut self, c: uint, src: Column) -> Column {
-        mem::replace(self.mut_c(c), src)
+        mem::replace(&mut (*self)[c], src)
     }
 
     /// Get a row from this array by-value.
@@ -80,13 +105,17 @@ pub trait Array2<Column: Array1<Element>, Row: Array1<Element>, Element: Copy> {
     fn swap_r(&mut self, a: uint, b: uint);
 
     /// Return a shared reference to the element at column `c` and row `r`.
+    #[deprecated = "Use index operators instead"]
     #[inline]
-    fn cr(&self, c: uint, r: uint) -> Element { self.c(c).i(r) }
+    fn cr<'a>(&'a self, c: uint, r: uint) -> &'a Element {
+        &(*self)[c][r]
+    }
 
     /// Return a mutable reference to the element at column `c` and row `r`.
+    #[deprecated = "Use index operators instead"]
     #[inline]
     fn mut_cr<'a>(&'a mut self, c: uint, r: uint) -> &'a mut Element {
-        self.mut_c(c).mut_i(r)
+        &mut (*self)[c][r]
     }
 
     /// Swap the values at index `a` and `b`
@@ -94,7 +123,7 @@ pub trait Array2<Column: Array1<Element>, Row: Array1<Element>, Element: Copy> {
     fn swap_cr(&mut self, a: (uint, uint), b: (uint, uint)) {
         let (ac, ar) = a;
         let (bc, br) = b;
-        unsafe { ptr::swap(self.mut_cr(ac, ar), self.mut_cr(bc, br)) };
+        unsafe { ptr::swap(&mut (*self)[ac][ar], &mut (*self)[bc][br]) };
     }
 
     /// Apply a function to each column.
